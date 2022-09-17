3d0eb7ff79f12ba093e8eb51b7e6f79bb1f6e1c0
nleyton <ggodot@gmail.com>
2022-09-17 23:05:04 +0300

    +ex05final

--- d00/ex05/concatenator.sh
+++ d00/ex05/concatenator.sh
@@ -1,2 +1,2 @@
 echo "\"id\",\"created_at\",\"name\",\"has_test\",\"alternate_url\"" > hh_positions.csv
-cat 20*.csv | sed -En '/^"id\",\"created_at\",\"name\",\"has_test\",\"alternate_url\"$/!p' >> hh_positions.csv
+cat *.csv | sed -En '/^"id\",\"created_at\",\"name\",\"has_test\",\"alternate_url\"$/!p' >> hh_positions.csv
